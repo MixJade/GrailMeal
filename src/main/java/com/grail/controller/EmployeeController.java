2e2d6893e68745d2312d1bfa1ae9aed21a33800c
MixJade <1114185977@qq.com>
2022-11-21 13:07:37 +0800

    MixJade

--- src/main/java/com/grail/controller/EmployeeController.java
+++ src/main/java/com/grail/controller/EmployeeController.java
@@ -35,31 +35,25 @@ public class EmployeeController {
      */
     @PostMapping("/login")
     public Result<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
-
         //1、将页面提交的密码password进行md5加密处理
         String password = employee.getPassword();
         password = DigestUtils.md5DigestAsHex(password.getBytes());
-
         //2、根据页面提交的用户名username查询数据库
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(Employee::getUsername,employee.getUsername());
         Employee emp = employeeService.getOne(queryWrapper);
-
         //3、如果没有查询到则返回登录失败结果
         if(emp == null){
             return Result.error("登录失败");
         }
-
         //4、密码比对，如果不一致则返回登录失败结果
         if(!emp.getPassword().equals(password)){
             return Result.error("登录失败");
         }
-
         //5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
         if(emp.getStatus() == 0){
             return Result.error("账号已禁用");
         }
-
         //6、登录成功，将员工id存入Session并返回登录成功结果
         request.getSession().setAttribute("employee",emp.getId());
         return Result.success(emp);
